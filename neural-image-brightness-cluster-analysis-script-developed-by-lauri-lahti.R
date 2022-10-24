a10c3fae3662312d3ba0ba5487774c33bfa60084
laurilahti <116580298+laurilahti@users.noreply.github.com>
2022-10-24 21:29:10 +0300

    Create neural-image-brightness-cluster-analysis-script-developed-by-lauri-lahti.R

---
+++ neural-image-brightness-cluster-analysis-script-developed-by-lauri-lahti.R
@@ -0,0 +1,17 @@
+#   "Neural image brightness cluster analysis script" developed, created and programmed by Lauri Lahti, Department of Computer Science, Aalto University School of Science, Finland (email: lauri.lahti@aalto.fi), first published on 15 July 2022 and openly available in the following document: Lahti, Lauri (2022). Developing ethical and transparent artificial intelligence algorithms to support decision making in healthcare based on brain research and personal care events of patients. Research article manuscript self-archived on 15 July 2022 at https://aaltodoc.aalto.fi/handle/123456789/115565. Open access: http://urn.fi/URN:NBN:fi:aalto-202207154400.
+
+#   This script can be used by anyone for non-commercial purposes while citing the just-mentioned research article (Lahti, Lauri, 2022) which provides further details.
+
+#   This current version (20221024a) of the script is completed and published by Lauri Lahti at https://github.com/laurilahti/neural-image-brightness-cluster-analysis-script on 24 October 2022.
+
+#   Please kindly note: The actual script is not included in this current version (20221024a) of the script and the actual script will be included in a later version of the script. This current version (20221024a) of the script is intended primarily for testing purposes. 
+
+
+#   A testing segment begins here.
+
+#   A testing about a R language script.
+
+print("A testing.")
+
+#   A testing segment ends here.
+
